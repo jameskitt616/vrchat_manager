c47be6c1812191155a585fbb600f1f076659b7a4
jameskitt616 <jameskitt@posteo.de>
2022-01-02 14:49:50 +0100

    Add user list and detailed user view with http requests

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Main());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,92 +16,280 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: const Home(title: 'Home'),
+      home: Home(),
     );
   }
 }
 
+// class User {
+//   final int id;
+//   final int userId;
+//   final String title;
+//   final String body;
+//
+//   User({
+//     required this.id,
+//     required this.userId,
+//     required this.title,
+//     required this.body,
+//   });
+// }
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+//
+// class _HomePageState extends State<HomePage> {
+// //Applying get request.
+//
+//   Future<List<User>> getRequest() async {
+//     //replace your restFull API here.
+//     String url = 'https://jsonplaceholder.typicode.com/posts';
+//     final response = await http.get(Uri.parse(url));
+//
+//     var responseData = json.decode(response.body);
+//
+//     //Creating a list to store input data;
+//     List<User> users = [];
+//     for (var singleUser in responseData) {
+//       User user = User(
+//           id: singleUser["id"],
+//           userId: singleUser["userId"],
+//           title: singleUser["title"],
+//           body: singleUser["body"]);
+//
+//       //Adding user to the list.
+//       users.add(user);
+//     }
+//     return users;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Home'),
+//           leading: Icon(
+//             Icons.get_app,
+//           ),
+//         ),
+//         body: Container(
+//           padding: EdgeInsets.all(10),
+//           child: FutureBuilder(
+//             future: getRequest(),
+//             builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+//               if (snapshot.data == null) {
+//                 return Container(
+//                   child: Center(
+//                     child: CircularProgressIndicator(),
+//                   ),
+//                 );
+//               } else {
+//                 return ListView.builder(
+//                   itemCount: snapshot.data.length,
+//                   itemBuilder: (ctx, index) => ListTile(
+//                     title: Text(snapshot.data[index].title),
+//                     subtitle: Text(snapshot.data[index].body),
+//                     contentPadding: EdgeInsets.only(bottom: 10),
+//                   ),
+//                 );
+//               }
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 class Home extends StatefulWidget {
-  const Home({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  String url = 'https://randomuser.me/api/?results=20';
+  List data = [];
+  bool _isDataLoading = true;
+
+  Future<List> _getUsers() async{
+
+    var response = await http.get(Uri.parse(url),
+      headers: {"Accept": 'application/json'},
+    );
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+
+      var listData = json.decode(response.body);
+      data = listData['results'];
+
+      _isDataLoading = false;
     });
+    return data;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getUsers();
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: _isDataLoading
+            ? const Center(
+            child: CircularProgressIndicator()
+        )
+            : ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, index){
+              return ListTile(
+                title: Text(data[index]['name']['first']+" "+data[index]['name']['last']),
+                subtitle: Text(data[index]['email']),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(data[index]['picture']['thumbnail']),
+                ),
+                onTap: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context)=> DetailPage(data[index])));
+                },
+              );
+            }
+        )
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final data;
+  DetailPage(this.data);
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(data['name']['first']),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                radius: 40.0,
+                backgroundColor: Colors.pink,
+                backgroundImage: NetworkImage(
+                    data['picture']['thumbnail']),
+              ),
+            ),
+            const Divider(
+              height: 40.0,
+              color: Colors.grey,
+            ),
             const Text(
-              'You have pushed the button this many times:',
+              'NAME',
+              style: TextStyle(
+                  letterSpacing: 2.0
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              data['name']['title'] +" "+ data['name']['first'] + " "+ data['name']['last'],
+              style: const TextStyle(
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            const Divider(
+              height: 30.0,
+              color: Colors.grey,
+            ),
+            const Text(
+              'USERNAME',
+              style: TextStyle(
+                  letterSpacing: 2.0
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              data['login']['username'],
+              style: const TextStyle(
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Divider(
+              height: 30.0,
+              color: Colors.grey,
+            ),
+            const Text(
+              'EMAIL',
+              style: TextStyle(
+                  letterSpacing: 2.0
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              data['email'],
+              style: const TextStyle(
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Divider(
+              height: 30.0,
+              color: Colors.grey,
+            ),
+            const Text(
+              'PHONE',
+              style: TextStyle(
+                  letterSpacing: 2.0
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              data['phone'],
+              style: const TextStyle(
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Divider(
+              height: 30.0,
+              color: Colors.grey,
+            ),
+
           ],
         ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
